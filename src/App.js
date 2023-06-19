d1ccda102003d3202cbe4b90864860e3f1851072
Mahmud2640 <mdmahmud484@gmail.com>
2023-06-19 12:21:40 +0600

    search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header/Header";
+import InputForm from "./components/inputForm/InputForm";
+import NoteCard from "./components/noteCard/NoteCard";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/notes")
+      // fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const searchText = e.target.searchText.value;
+    if (searchText) {
+      fetch(`http://localhost:5000/notes?name=${searchText}`)
+        .then((res) => res.json())
+        .then((data) => setNotes(data));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleSearch={handleSearch} />
+      <InputForm />
+      <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
+        {notes.map((note) => (
+          <NoteCard note={note} />
+        ))}
+      </div>
     </div>
   );
 }
